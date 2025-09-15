1855af49fcc77ab98381d58d5110e84ccc43065c
itnn <61962532+itnn@users.noreply.github.com>
2025-09-15 12:59:38 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,13 +2,13 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = '1339d12e-327f-4dc7-b8bf-c0e603de158f';
+let userID = 'cf3c18fc-aa13-4f49-9b6a-be5a68dda1e1';
 
 // 生成配置文件的 Cloudflare 优选 IP (www.gov.se   speed.cloudflare.com等使用Cloudflare CDN的网站也可以)
 const bestCFIP = "www.visa.com.hk"
 
 // 用于 Cloudflare 网站的代理 IP
-const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org'];
+const proxyIPs = ['', ''];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://cloudflare-dns.com/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
